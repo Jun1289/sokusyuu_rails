9d8f5256a3b33c1044354d77409fd8f15196964d
Junpei <jun@note-Computer.local>
2023-07-29 18:19:39 +0900

    タスクのエクスポートとインポートを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,12 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = current_user.tasks.recent
+    @tasks = current_user.tasks
+
+    respond_to do |format|
+      format.html
+      format.csv {send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%S')}.csv"}
+    end
   end
 
   def show
@@ -43,7 +48,12 @@ class TasksController < ApplicationController
     @task.destroy
     redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
-
+  
+  def import 
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました"
+  end
+  
   private
 
   def task_params
