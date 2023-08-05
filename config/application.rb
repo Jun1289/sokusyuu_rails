f7694c3cb73898d7fe9bf834a6e741cb75fa5c45
Junpei <jun@note-Computer.local>
2023-08-05 21:28:30 +0900

    change files for connecting to db

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Taskleaf
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << 'xxx.onrender.com'
   end
 end
