75fdc3357906c21b42dcd57b27df43945b2fd2b8
Junpei <jun@note-Computer.local>
2023-08-05 18:45:15 +0900

    chage for connecting to DB
    change files for connecting to db
    
    change for host

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module Taskleaf
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << '"rails-deploy-test.onrender.com"'
+    config.hosts << 'rails-deploy-test.onrender.com'
   end
 end
