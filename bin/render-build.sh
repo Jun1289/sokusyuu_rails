3908b6543291d5da091f7f6b77694b773e862ba8
Junpei <jun@note-Computer.local>
2023-08-08 15:41:34 +0900

    edit for activestorage

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,4 +9,5 @@ bundle exec rake assets:precompile # cssはsprocketsを使っているため
 bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
 bundle exec rake assets:clean
 bundle exec rails active_storage:install
+bundle install
 bundle exec rails db:migrate
