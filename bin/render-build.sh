b2b61418e09a13f573477c0af7bef82186eae186
Junpei <jun@note-Computer.local>
2023-08-08 10:29:29 +0900

    install activestorage before migrate

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,5 +8,5 @@ yarn build # jsファイルをesbuildでバンドルしているため
 bundle exec rake assets:precompile # cssはsprocketsを使っているため
 bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
 bundle exec rake assets:clean
-bundle exec rails db:migrate
 rails active_storage:install
+bundle exec rails db:migrate
