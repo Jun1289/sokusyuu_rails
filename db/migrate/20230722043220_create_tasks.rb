430afc1c31058986c461943529960c6ee920d3de
Junpei <jun@note-Computer.local>
2023-08-13 13:06:21 +0900

    DBテーブルがない時のみ、作成を実行する

--- db/migrate/20230722043220_create_tasks.rb
+++ db/migrate/20230722043220_create_tasks.rb
@@ -1,12 +1,14 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
-    create_table :tasks do |t|
-      t.string :name
-      t.text :description
+    unless table_exists?(:tasks)
+      create_table :tasks do |t|
+        t.string :name
+        t.text :description
 
-      # t.timestamps
-      t.datetime :created_at, precision: 6
-      t.datetime :updated_at, precision: 6
+        # t.timestamps
+        t.datetime :created_at, precision: 6
+        t.datetime :updated_at, precision: 6
+      end
     end
   end
 end
--- db/migrate/20230723041541_create_users.rb
+++ db/migrate/20230723041541_create_users.rb
@@ -1,14 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :users do |t|
-      t.string :name, null: false
-      t.string :email, null: false
-      t.string :password_digest, null: false
+    unless table_exists?(:users)
+      create_table :users do |t|
+        t.string :name, null: false
+        t.string :email, null: false
+        t.string :password_digest, null: false
 
-      # t.timestamp
-      t.datetime :created_at, precision: 6
-      t.datetime :updated_at, precision: 6
-      t.index :email, unique: true
+        # t.timestamp
+        t.datetime :created_at, precision: 6
+        t.datetime :updated_at, precision: 6
+        t.index :email, unique: true
+      end
     end
   end
 end
